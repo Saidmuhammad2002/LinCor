228e44e7a398147f6cbf71440a56c10f2ac56761
Guy <efrat.gay@abutech.uz>
2023-02-24 02:04:01 +0500

    feat/home-video added youtube video

---
+++ src/components/sections/home-video/home-video.tsx
@@ -0,0 +1,18 @@
+import "./_home-video.scss";
+
+export const HomeVideo = () => {
+  return (
+    <div className="container">
+      <iframe
+        width="100%"
+        height="604"
+        src="https://www.youtube.com/embed/Huo7mRiOXcI"
+        title="The Batman of London."
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+        style={{ borderRadius: "20px" }}
+      ></iframe>
+    </div>
+  );
+};
