2f19e0a8d7ac31b025e732024926466c46e9b836
Guy <efrat.gay@abutech.uz>
2023-02-28 23:15:01 +0500

    feat/split private & public pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { Route, Routes } from "react-router-dom";
-import { MainLayout } from "./components/layouts";
-import { Home } from "./pages";
 import { Private } from "./private.app";
 import { Public } from "./public.app";
 
 export const App: React.FC = () => {
-  return (
-    <MainLayout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </MainLayout>
-  );
+  const loggedIn = false;
+  if (loggedIn) {
+    return <Private />;
+  }
+  return <Public />;
 };
