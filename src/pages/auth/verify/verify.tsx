bafa3ea9402c313b8676776784e4a8c36ef8a5f7
Guy <efrat.gay@abutech.uz>
2023-03-03 23:31:58 +0500

    feat(verify-phone) some changes

--- src/pages/auth/verify/verify.tsx
+++ src/pages/auth/verify/verify.tsx
@@ -1,5 +1,21 @@
 import "./_verify.scss";
 
 export const Verify = () => {
-  return <div>Verify</div>;
+  return (
+    <div className="login">
+      <h3 className="login__logo">LinCor</h3>
+      <h2 className="login__heading">Tasdiqlash</h2>
+      <p className="login__advice">
+        Shu raqamga yuborilgan maxfiy kodni kiriting.
+      </p>
+
+      <form className="login__form">
+        <label className="login__label">
+          Kodni kiriting
+          <input type="text" className="login__controls" />
+          <button>Kodni qayta yuborish</button>
+        </label>
+      </form>
+    </div>
+  );
 };
