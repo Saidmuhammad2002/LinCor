e41b61fdab81b16969da14281b661973fb41101c
Guy <efrat.gay@abutech.uz>
2023-03-09 21:30:51 +0500

    feat(not-found): not-found page

--- src/private.app.tsx
+++ src/private.app.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { MainLayout } from "./components/layouts";
-import { Home } from "./pages";
+import { Home, NotFound } from "./pages";
 
 export const Private: React.FC = () => {
   return (
     <MainLayout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </MainLayout>
   );
