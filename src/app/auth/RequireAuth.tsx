e1ddec32a12afeb1633f31e5116bff21e0c689a3
Saidmuhammad2002 <sololearner567@gmail.com>
2023-03-28 16:11:33 +0500

    latest change for auth

--- src/app/auth/RequireAuth.tsx
+++ src/app/auth/RequireAuth.tsx
@@ -2,10 +2,10 @@ import { useAuth } from '@utils/hooks';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
 const RequireAuth = () => {
-  let { email } = useAuth();
+  let user = useAuth();
   let location = useLocation();
 
-  const content = email ? (
+  const content = user ? (
     <Outlet />
   ) : (
     <Navigate to="/login" state={{ from: location }} replace />
